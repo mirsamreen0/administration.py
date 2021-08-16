94921fec202df19cea032c4a2d68de61cd1e5348
mirsamreen0 <88386505+mirsamreen0@users.noreply.github.com>
2021-08-16 20:39:50 +0530

    Add files via upload

---
+++ schoolad.py
@@ -0,0 +1,54 @@
+students=["Wamiq","Amaar","Amaan"]
+students.append("Khalid")
+print(students)
+
+
+students=["Wamiq","Amaar","Amaan"]
+students.clear()
+print(students)
+
+
+students=["Wamiq","Amaar","Amaan"]
+x = students.copy()
+print(x)
+
+
+
+students=["Wamiq","Amaar","Amaan"]
+x = students.count("Amaan")
+print(x)
+
+
+students=["Wamiq","Amaar","Amaan"]
+teachers = ['Aadil','Khubaib ','Younis']
+students.extend(teachers)
+print(students)
+
+
+students=["Wamiq","Amaar","Amaan"]
+x = students.index("Amaan")
+print(x)
+
+
+students=["Adnan","Basit","Amaan"]
+students.insert(1,"Arsalan")
+print(students)
+
+students=["Wamiq","Amaar","Amaan"]
+students.pop(1)
+print(students)
+
+
+
+students=["Wamiq","Amaar","Amaan"]
+students.remove("Amaar")
+print(students)
+
+students=["Wamiq","Amaar","Amaan"]
+students.reverse()
+print(students)
+
+teachers = ['Aadil','Khubaib ','Younis']
+teachers.sort()
+print(teachers)
+
